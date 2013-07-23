84687046061d49268360f4560f5e18c35b9cfe6a
Chris Castiglione <castig@gmail.com>
2013-07-22 17:36:09 -0700

    added wysihtml5 gem

--- Gemfile
+++ Gemfile
@@ -47,3 +47,9 @@ end
 #added 
 gem 'bootstrap-sass', '~> 2.3.2.0'
 gem 'simple_form', '~> 3.0.0.rc'
+gem 'bootstrap-wysihtml5-rails'
+
+
+
+
+
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require bootstrap 
+//= require bootstrap-wysihtml5
 //= require_tree .
