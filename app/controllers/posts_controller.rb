c939e99885191fdff631b6a790fadbf315653965
Chris Castiglione <castig@gmail.com>
2013-07-22 19:12:50 -0700

    authenticate_user in the posts controller so that we can't view the edit page when not logged in

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,8 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+
+
 
   # GET /posts
   # GET /posts.json
